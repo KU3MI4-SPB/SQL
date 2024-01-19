ddb9200deb258c116c116a8d24c7367852f6cc5b
KU3MI4 <kuzmichov@bk.ru>
2024-01-19 20:21:07 +0300

    add task1 hw2

---
+++ Home_Work_2/task1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE itresume9457367.manufacturer (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO itresume9457367.manufacturer (name)
+VALUES 
+('Apple'),
+('Samsung'),
+('Huawei');
