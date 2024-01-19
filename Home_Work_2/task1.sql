27549dbc0a482be5adff8052078ddc88af6a22ca
KU3MI4 <kuzmichov@bk.ru>
2024-01-19 20:28:59 +0300

    edit task1 hw2

--- Home_Work_2/task1.sql
+++ Home_Work_2/task1.sql
@@ -1,9 +1,9 @@
-CREATE TABLE itresume9457367.manufacturer (
+CREATE TABLE itresumeXXXXXXX.manufacturer (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
-INSERT INTO itresume9457367.manufacturer (name)
+INSERT INTO itresumeXXXXXXX.manufacturer (name)
 VALUES 
 ('Apple'),
 ('Samsung'),
